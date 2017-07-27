3c2d44c1d4e77e13a45d60c9756029d6fd411c86
pennetti <tjp7mb@virginia.edu>
2017-07-27 08:25:20 -0400

    Fix voicebox API endpoint

--- client/api.js
+++ client/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default function addCorpus(text) {
-    return axios.post('/src', { text })
+    return axios.post('/voicebox', { text })
         .then(response => response.data);
 }
